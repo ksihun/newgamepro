821e5da25ab638f3268398d74696c555c28d92c7
ksihun <58981473+ksihun@users.noreply.github.com>
2020-08-03 00:57:41 +0900

    enemy movement
    dfd

--- Assets/scripts/attack.cs
+++ Assets/scripts/attack.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class attack : MonoBehaviour
+public class Attack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int dmg;
 
-    // Update is called once per frame
-    void Update()
+
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.tag == "BorderAttack")
+        {
+            Destroy(gameObject);
+
+        }
     }
 }
